167e9b2d4b6405489e2f933fcab381d62da64126
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-05-31 18:21:24 +0200

    add profile and movie pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,45 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { React, useState } from "react";
-import Landing from "./components/Landing";
+import Landing from "./pages/Landing";
 import Header from "./components/Header";
 import Login from "./components/Login";
+import Movie from "./pages/Movie";
+import Profile from "./pages/Profile";
 import Redirect from "./components/Redirect";
 
 function App() {
-	const server = "http://localhost:4000/api";
-	const url = "http://localhost:3000";
-	const client_id =
-		"893943746591-0lo8ecf9m5quo67p1e0h9bfa5a05082j.apps.googleusercontent.com";
-	const [domain, setDomain] = useState("");
+  const server = "http://localhost:4000/api";
+  const url = "http://localhost:3000";
+  const client_id =
+    "893943746591-0lo8ecf9m5quo67p1e0h9bfa5a05082j.apps.googleusercontent.com";
+  const [domain, setDomain] = useState("");
 
-	return (
-		<Router>
-			<Header setDomain={setDomain} />
-			<Routes>
-				<Route
-					path={`/`}
-					exact
-					element={
-						<Landing server={server} domain={domain} setDomain={setDomain} />
-					}
-				/>
-				<Route
-					path={`/login`}
-					exact
-					element={<Login server={server} client_id={client_id} url={url} />}
-				/>
-				<Route
-					path={`/redirect`}
-					exact
-					element={<Redirect server={server} client_id={client_id} url={url} />}
-				/>
-			</Routes>
-		</Router>
-	);
+  return (
+    <Router>
+      <Header setDomain={setDomain} />
+      <Routes>
+        <Route
+          path={`/`}
+          exact
+          element={
+            <Landing server={server} domain={domain} setDomain={setDomain} />
+          }
+        />
+        <Route path="/movie" element={<Movie />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route
+          path={`/login`}
+          exact
+          element={<Login server={server} client_id={client_id} url={url} />}
+        />
+        <Route
+          path={`/redirect`}
+          exact
+          element={<Redirect server={server} client_id={client_id} url={url} />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import message from "./message";
+import message from "../components/message";
 const axios = require("axios");
 
 const Landing = ({ server, domain, setDomain }) => {
   const navigate = useNavigate();
 
   const testFetch = async () => {
-    let resp = await axios
-      .get
-      //one movie by id main details
-      //"https://api.themoviedb.org/3/movie/406?api_key=ecab10a318d6e5505711563709a6cd40"
-      //one movie by id cast and crew
-      //"https://api.themoviedb.org/3/movie/406/credits?api_key=ecab10a318d6e5505711563709a6cd40"
-      //search
-      //"https://api.themoviedb.org/3/search/movie?api_key=ecab10a318d6e5505711563709a6cd40&query=la haine&page=1"
-      //default list
-      //"https://api.themoviedb.org/3/discover/movie?api_key=ecab10a318d6e5505711563709a6cd40&sort_by=popularity.desc&page=1"
-      ();
+    let resp = await axios.get(
+      "https://api.themoviedb.org/3/search/movie?api_key=ecab10a318d6e5505711563709a6cd40&query=la haine&page=1"
+    );
     console.log(resp.data);
     //console.log(resp.data.results[0]);
   };
+
+  //one movie by id main details
+  //"https://api.themoviedb.org/3/movie/406?api_key=ecab10a318d6e5505711563709a6cd40"
+  //one movie by id cast and crew
+  //"https://api.themoviedb.org/3/movie/406/credits?api_key=ecab10a318d6e5505711563709a6cd40"
+  //default list
+  //"https://api.themoviedb.org/3/discover/movie?api_key=ecab10a318d6e5505711563709a6cd40&sort_by=popularity.desc&page=1"
 
   /* const sendRequest = async (url) => {
 		try {
@@ -47,14 +46,15 @@ const Landing = ({ server, domain, setDomain }) => {
   return (
     <div>
       <button onClick={() => testFetch()}>TEST</button>
-      <img
+      {/*       <img
         src="https://image.tmdb.org/t/p/original/bW4tXG8kZ89ZCurPnzDDSzJbeF5.jpg"
         alt="movie pic"
       />
       <img
         src="https://image.tmdb.org/t/p/original/kuxI08YpwQFGweIXK7TELknwexr.jpg"
         alt="saiid pic"
-      />
+      /> */}
+      <button onClick={() => navigate("/movie")}>Movie</button>
     </div>
   );
 };
