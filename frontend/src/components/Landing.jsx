99d5c8260ee697cdab5771d872625459e7263ce3
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-05-31 18:01:00 +0200

    add api-fetch tests

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import message from "./message";
+const axios = require("axios");
 
 const Landing = ({ server, domain, setDomain }) => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	/* const sendRequest = async (url) => {
+  const testFetch = async () => {
+    let resp = await axios
+      .get
+      //one movie by id main details
+      //"https://api.themoviedb.org/3/movie/406?api_key=ecab10a318d6e5505711563709a6cd40"
+      //one movie by id cast and crew
+      //"https://api.themoviedb.org/3/movie/406/credits?api_key=ecab10a318d6e5505711563709a6cd40"
+      //search
+      //"https://api.themoviedb.org/3/search/movie?api_key=ecab10a318d6e5505711563709a6cd40&query=la haine&page=1"
+      //default list
+      //"https://api.themoviedb.org/3/discover/movie?api_key=ecab10a318d6e5505711563709a6cd40&sort_by=popularity.desc&page=1"
+      ();
+    console.log(resp.data);
+    //console.log(resp.data.results[0]);
+  };
+
+  /* const sendRequest = async (url) => {
 		try {
 			const res = await axios(`${server}${url}`, {
 				headers: {
@@ -28,7 +44,19 @@ const Landing = ({ server, domain, setDomain }) => {
 		}
 	}; */
 
-	return <div></div>;
+  return (
+    <div>
+      <button onClick={() => testFetch()}>TEST</button>
+      <img
+        src="https://image.tmdb.org/t/p/original/bW4tXG8kZ89ZCurPnzDDSzJbeF5.jpg"
+        alt="movie pic"
+      />
+      <img
+        src="https://image.tmdb.org/t/p/original/kuxI08YpwQFGweIXK7TELknwexr.jpg"
+        alt="saiid pic"
+      />
+    </div>
+  );
 };
 
 export default Landing;
