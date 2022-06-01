aa7b0f2d3302c8b285ebac6edc3c3132a881ce91
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-06-01 14:26:19 +0200

    change movie crew code

--- frontend/src/components/MovieCrew.jsx
+++ frontend/src/components/MovieCrew.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const MovieCrew = ({ data }) => {
   const buildActorCard = (actor, i) => {
-    console.log("ACTOR!");
     const actorPic = "https://image.tmdb.org/t/p/original" + actor.profile_path;
     return (
       <div className="actor-card" key={i}>
@@ -15,25 +14,22 @@ const MovieCrew = ({ data }) => {
     );
   };
 
+  const getMainCrew = (member, i, job) => {
+    if (member.job === job)
+      return (
+        <p key={i} className="movie-crew-director">
+          {member.name}
+        </p>
+      );
+  };
+
   return (
     <div className="movie-crew-container">
       <div className="director-container">
-        {data.crew.map(
-          (member, i) =>
-            member.job === "Director" && (
-              <p key={i} className="movie-crew-director">
-                Director: {member.name}
-              </p>
-            )
-        )}
-        {data.crew.map(
-          (member, i) =>
-            member.job === "Screenplay" && (
-              <p key={i} className="movie-crew-screenplay">
-                Screenplay: {member.name}
-              </p>
-            )
-        )}
+        <p>Director:</p>
+        {data.crew.map((member, i) => getMainCrew(member, i, "Director"))}
+        <p>Screenplay:</p>
+        {data.crew.map((member, i) => getMainCrew(member, i, "Screenplay"))}
       </div>
       <div className="actor-container">
         {data.cast.map((actor, i) => buildActorCard(actor, i))}
--- frontend/src/components/MovieReviews.jsx
+++ frontend/src/components/MovieReviews.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
 
 const MovieReviews = ({ movieId }) => {
-  return <div>Reviews....</div>;
+  const [loggedin, setLoggedin] = useState(false);
+
+  const isLoggedIn = async () => {
+    const token = sessionStorage.getItem("token");
+    const decoded = await jwt_decode(token);
+    console.log("USER-INFO: ", decoded);
+    decoded ? setLoggedin(true) : setLoggedin(false);
+  };
+
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
+  return <div>{loggedin ? "LOGGED-IN" : "NOT-LOOGGED-IN"}</div>;
 };
 
 export default MovieReviews;
